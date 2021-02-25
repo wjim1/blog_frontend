9524bf69117e1c29c9a610d0f0a8e9e422bdff6a
wj <969576137@qq.com>
2021-02-25 21:00:53 +0800

    change node name

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -19,10 +19,10 @@
             <!--<a class="nav-link" href="#">问答</a>-->
             <!--</li>-->
             <router-link :to="{ name: 'nodes.show' }" exact tag="li" class="nav-item">
-              <a href="javascript:void(0);" class="nav-link">节点</a>
+              <a href="javascript:void(0);" class="nav-link">讨论</a>
             </router-link>
             <li class="nav-item">
-              <a class="nav-link" href="https://easywechat.com?utm_source=yike.io">SDK</a>
+              <a class="nav-link" href="https://easywechat.com?utm_source=yike.io">问答</a>
             </li>
             <li class="nav-item">
               <nav-search />
