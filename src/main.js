f3b2991b5d1251f65b475eccb38875a3a7beaedb
wj <969576137@qq.com>
2021-02-25 16:51:11 +0800

    add bootstrap depend

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min'
 import router from './router'
 
 Vue.config.productionTip = false
