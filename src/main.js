511b425eaf774cdb8575d16ebd2b6091a2c1f0bd
wj <969576137@qq.com>
2021-02-25 18:19:12 +0800

    import all style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.min'
 import router from './router'
