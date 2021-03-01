39a6e4a90f703f33d41c4077986651be4e6437e0
wj <969576137@qq.com>
2021-03-01 14:43:19 +0800

    import vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import './bootstrap'
 import axios from 'axios'
 import App from './App.vue'
+import store from './store/index'
 import router from './router'
 import VueAxios from 'vue-axios'
 import { Message } from 'element-ui'
@@ -12,6 +13,7 @@ import 'element-ui/lib/theme-chalk/message.css'
 Vue.use(VueAxios, axios)
 Vue.prototype.$message = Message
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
