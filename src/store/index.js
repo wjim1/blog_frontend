a605fab5b18f18b5e6826f2578ad7a63ad728dfb
wj <969576137@qq.com>
2021-03-02 16:50:14 +0800

    login

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import request from '../utils/http/request'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: {},
+    token: null
   },
   mutations: {
-
+    setUser (state, value) {
+      this.state.user = value
+    },
+    setToken (state, value) {
+      this.state.token = value
+    }
   },
   actions: {
-
+    attemptLogin (context, value) {
+      return request({
+        url: 'auth/login',
+        method: 'post',
+        params: value
+      })
+    }
   }
 })
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,11 +1,64 @@
 <template>
-
+  <div class="row pt-3">
+    <div class="offset-sm-4 col-sm-4">
+      <div class="box">
+        <h4 class="text-center font-weight-normal mt-2">用户登录</h4>
+        <form @submit.prevent="submit">
+          <div class="form-group">
+            <label>邮箱<small></small></label>
+            <input type="text" class="form-control" v-model="email" required>
+          </div>
+          <div class="form-group">
+            <label>密码</label>
+            <input type="password" class="form-control" v-model="password" required>
+          </div>
+          <button type="submit" class="my-2 btn btn-primary w-100">登录</button>
+        </form>
+        <div class="mb-2 d-flex justify-content-around">
+          <!--<socialite-btns></socialite-btns>-->
+        </div>
+      </div>
+    </div>
+    <div class="offset-sm-4 col-sm-4 text-center mt-2 d-flex justify-content-between">
+      <p>没有账号？ <router-link class="text-blue" :to="{ name: 'auth.register' }">快速注册</router-link>
+      </p>
+      <p>
+        <router-link class="text-blue" :to="{ name: 'auth.forget-password' }">忘记密码？</router-link>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'login'
+import { mapActions } from 'vuex'
+export default {
+  name: 'login',
+  components: {},
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    ...mapActions(['attemptLogin']),
+    async submit () {
+      const params = {
+        email: this.email,
+        password: this.password
+      }
+      try {
+        await this.attemptLogin(params)
+        this.$message.success('欢迎回来~')
+        this.$router.push({ name: 'home' })
+      } catch (e) {
+        if (e.status !== 422) {
+          this.$message.error('账号密码错误!')
+        }
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
